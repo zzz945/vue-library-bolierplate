5d329c281465dbf5e42e8de069e912cf65909043
zhangdaiyan <zhangdaiyan@vipkid.com.cn>
2018-03-08 16:33:50 +0800

    Done

---
+++ README.md
@@ -0,0 +1,15 @@
+# What is this
+A boilerplate for creating vue component library
+## Usage
+```
+git clone git@github.com:zzz945/vue-library-bolierplate.git
+npm install
+```
+## Dev
+```
+npm run dev
+```
+## Prod
+```
+npm run build
+```
---
+++ src/main.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .library-example {{content}}
+</template>
+
+<script>
+  export default {
+    name: 'library-example',
+
+		components: {
+		},
+
+    props: {
+    },
+
+    data () {
+      return {
+        content: "Hello vue library boilerplate!"
+      }
+    },
+
+		watch: {
+    },
+
+    computed: {
+    },
+
+    methods: {
+    },
+
+    mounted() {
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .library-example
+    font-size: 20px
+</style>
